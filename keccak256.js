c4395815627989ed733ea3fe510d2b764e3a58dc
zillerium <23511764+zillerium@users.noreply.github.com>
2023-08-22 18:19:25 +0100

    Create keccak256.js

---
+++ keccak256.js
@@ -0,0 +1,8 @@
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+    return keccak256(utf8ToBytes(message));
+}
+
+module.exports = hashMessage;
