73c32c87f038792f80246cd89ef86cde3ea84cf6
zillerium <23511764+zillerium@users.noreply.github.com>
2023-08-27 10:13:55 +0100

    Create insufficientcheck.js

---
+++ spend/insufficientcheck.js
@@ -0,0 +1,23 @@
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+        this.inputUTXOs=inputUTXOs;
+        this.outputUTXOs=outputUTXOs;
+        
+    }
+    execute() {
+        for (let utxo of this.inputUTXOs) {
+            if (utxo.spent) {
+                throw new Error('Double-spending transaction output detected!');
+            }
+        }
+
+        let inputTotal = this.inputUTXOs.reduce((total, txo) => total + txo.amount, 0);
+        let outputTotal = this.outputUTXOs.reduce((total, txo) => total + txo.amount, 0);
+
+        if (inputTotal < outputTotal) {
+            throw new Error('Insufficient input value!');
+        }
+    }
+}
+
+module.exports = Transaction;
