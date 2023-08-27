1086b8a6fd7992340056b5169b99b4020f08d921
zillerium <23511764+zillerium@users.noreply.github.com>
2023-08-27 10:17:05 +0100

    Create txn.js

---
+++ spend/txn.js
@@ -0,0 +1,27 @@
+class Transaction {
+    constructor(inputUTXOs, outputUTXOs) {
+        this.inputUTXOs=inputUTXOs;
+        this.outputUTXOs=outputUTXOs;
+        
+    }
+    execute() {
+        for (let utxo of this.inputUTXOs) {
+            if (utxo.spent) {
+                throw new Error('Double-spending transaction output detected!');
+            }
+        }
+
+        let inputTotal = this.inputUTXOs.reduce((total, txo) => total + txo.amount, 0);
+        let outputTotal = this.outputUTXOs.reduce((total, txo) => total + txo.amount, 0);
+
+        if (inputTotal < outputTotal) {
+            throw new Error('Insufficient input value!');
+        }
+
+        for (let txo of this.inputUTXOs) {
+            txo.spend();
+        }
+    }
+}
+
+module.exports = Transaction;
