5a327addb997c0957605117fe64ca5030652dbf1
zillerium <23511764+zillerium@users.noreply.github.com>
2023-08-29 09:29:50 +0100

    Create getBlock.js

---
+++ ethereum/getBlock.js
@@ -0,0 +1,21 @@
+const axios = require('axios');
+
+// grab the API key from the .env
+require('dotenv').config();
+const url = `https://eth-mainnet.g.alchemy.com/v2/${process.env.API_KEY}`;
+
+async function getBlockNumber() {
+    const response = await axios.post(url, {
+        jsonrpc: "2.0",
+        id: 1,
+        method: "eth_blockNumber", // <-- TODO: fill in the method name
+    });
+
+    // axios has a data prop which is the response from the server
+    // TOOD: use this console log to locate the block number
+    console.log(response.data); 
+return response.data.result;
+    // TODO: return the block number
+}
+
+module.exports = getBlockNumber;
