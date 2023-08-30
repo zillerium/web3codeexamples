43b64bfd8fef2edc35018703587b441b9a984919
zillerium <23511764+zillerium@users.noreply.github.com>
2023-08-30 06:21:53 +0100

    Create findbal.js

---
+++ ethereum/findbal.js
@@ -0,0 +1,13 @@
+const { Wallet, providers } = require('ethers');
+const { ganacheProvider } = require('./config');
+
+const provider = new providers.Web3Provider(ganacheProvider);
+async function findMyBalance(privateKey) {
+    // retrieve the balance, given a private key
+    const wallet = new Wallet(privateKey, provider);
+
+    const balance = await wallet.getBalance();
+    return balance;
+}
+
+module.exports = findMyBalance;
