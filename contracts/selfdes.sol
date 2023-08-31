9a76ec4acce316b3b30ca977a9b41c5bc44761b8
zillerium <sales@merrows.co.uk>
2023-08-31 07:20:54 +0000

    self

---
+++ contracts/selfdes.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+ 
+
+contract Contract {
+    address public owner;
+    address public charity;
+
+    constructor(address _charity) {
+        owner = msg.sender;
+        charity = _charity;
+    }
+
+    receive() external payable {
+        //console.log(msg.value); // 100000
+    }
+    
+    function tip() public payable {
+        //console.log(msg.value); // 100000
+        owner.call{value: msg.value}("");
+    }
+
+    function donate() public {
+         payable(charity).transfer(address(this).balance);
+         selfdestruct(payable(msg.sender));
+
+
+    }
+}
